5eed7c3c4e0a54af9fa5ee32338434b054fb9b0b
noobiwankenoobi <ross.hazelett@gmail.com>
2018-11-29 16:10:29 -0500

    jquery is running, button does hide and show

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,3 +1,28 @@
 'use strict'
 
-module.exports = true
+let testDivState = 'hidden'
+
+// if testDivState is hidden, then run .show and set testDivState as shown
+// if testDivState is shown, then run .hide and set testDivState to hidden
+
+const onTestButtonClick = () => {
+  if (testDivState === 'hidden') {
+    $('#test-div').show()
+    testDivState = 'shown'
+    console.log('first if working')
+  } else if (testDivState === 'shown') {
+    $('#test-div').hide()
+    testDivState = 'hidden'
+    console.log('second if working')
+  } else {
+    console.log('unknown state of testDivState')
+  }
+}
+
+const addHandlers = () => {
+  $('#test-button').on('click', onTestButtonClick)
+}
+
+module.exports = {
+  addHandlers
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,13 +2,11 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const example = require('./example')
+
+$('#test-div').hide()
 
 $(() => {
   setAPIOrigin(location, config)
+  example.addHandlers()
 })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-require('./example')
