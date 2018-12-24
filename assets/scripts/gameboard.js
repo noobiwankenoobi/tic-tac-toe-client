72622eeef598249af3941122993ee6646e20b633
noobiwankenoobi <ross.hazelett@gmail.com>
2018-12-23 23:29:15 -0500

    added game cell states etc

---
+++ assets/scripts/gameboard.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let gameBoardState = 'clear'
+
+let gameCellOneState = 'empty'
+let gameCellTwoState = 'empty'
+let gameCellThreeState = 'empty'
+let gameCellFourState = 'empty'
+let gameCellFiveState = 'empty'
+let gameCellSixState = 'empty'
+let gameCellSevenState = 'empty'
+let gameCellEightState = 'empty'
+let gameCellNineState = 'empty'
+
+const addHandlers = () => {
+  // $('#test-button').on('click', onTestButtonClick)
+}
+
+module.exports = {
+  addHandlers
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const example = require('./example')
+const gameboard = require('./gameboard.js')
 
 $('#test-div').hide()
 
 $(() => {
   setAPIOrigin(location, config)
   example.addHandlers()
+  gameboard.addHandlers()
 })
